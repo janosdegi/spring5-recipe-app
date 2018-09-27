413d3f7566390e09104f441ed030fbc74d3ebec3
Dégi János <johann.degi@gmail.com>
2018-09-27 15:36:18 +0200

    a

--- src/main/java/guru/springframework/domain/Notes.java
+++ src/main/java/guru/springframework/domain/Notes.java
@@ -8,7 +8,6 @@ import javax.persistence.*;
 /**
  * Created by jt on 6/13/17.
  */
-@Data
 @EqualsAndHashCode(exclude = {"recipe"})
 @Entity
 public class Notes {
@@ -23,4 +22,28 @@ public class Notes {
     @Lob
     private String recipeNotes;
 
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Recipe getRecipe() {
+        return recipe;
+    }
+
+    public void setRecipe(Recipe recipe) {
+        this.recipe = recipe;
+    }
+
+    public String getRecipeNotes() {
+        return recipeNotes;
+    }
+
+    public void setRecipeNotes(String recipeNotes) {
+        this.recipeNotes = recipeNotes;
+    }
 }
