39fb6bc350d2301a356c8c98a20129c89d80023a
Dégi János <johann.degi@gmail.com>
2018-10-01 12:19:56 +0200

    Integration Test

--- src/main/java/guru/springframework/domain/UnitOfMeasure.java
+++ src/main/java/guru/springframework/domain/UnitOfMeasure.java
@@ -10,7 +10,7 @@ import javax.persistence.Id;
 /**
  * Created by jt on 6/13/17.
  */
-@Data
+
 @Entity
 public class UnitOfMeasure {
 
@@ -18,4 +18,20 @@ public class UnitOfMeasure {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String description;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
